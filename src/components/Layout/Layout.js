77e3d1566e92f72e4e151ceb6f7637a9676f185a
Ngô Thanh Sơn <songas000@gmail.com>
2020-10-09 10:41:14 +0700

    n

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,13 @@ import React from "react";
 import Aux from "../../hoc/Auxilary";
 
 import classes from "./Layout.module.css";
-
 import ToolBar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = (props) => (
   <Aux>
     <ToolBar></ToolBar>
+    <SideDrawer></SideDrawer>
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
